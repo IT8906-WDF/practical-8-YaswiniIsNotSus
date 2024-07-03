fa9a3ed4bb86ae6cccc7f57d563173314f3be122
YaswiniIsNotSus <148456051+YaswiniIsNotSus@users.noreply.github.com>
2024-07-03 12:46:54 +0000

    mega update

--- src/RockPaperScissors.js
+++ src/RockPaperScissors.js
@@ -2,136 +2,37 @@ import RPSButtons from './RPSButtons.js';
 import RPSInput from './RPSInput.js';
 import RPSRecords from './RPSRecords.js';
 import React from 'react';
+import React from 'react';
 import recordsReducer from './recordsReducer';
+import { IsEmojiContext } from './IsEmojiContext';
 
-const moves = ['Rock', 'Paper', 'Scissors'];
-const IsEmojiContext = React.createContext(false);
-const emoji = { Rock: '🗿', Paper: '📝', Scissors: '✂️' };
-
-//RPSInput.js
-function RPSInput(props) {
-  const [selectedMove, setSelectedMove] = React.useState('Rock');
-  const [isWin, setIsWin] = React.useState(false);
-  return (
-    <div>
-      <select
-        value={selectedMove}
-        onChange={(e) => setSelectedMove(e.target.value)}
-      >
-        <option value="Rock">Rock</option>
-        <option value="Paper">Paper</option>
-        <option value="Scissors">Scissors</option>
-      </select>
-      <input
-        type="checkbox"
-        checked={isWin}
-        onChange={(e) => setIsWin(e.target.checked)}
-      />{' '}
-      Win?
-      <button
-        onClick={() =>
-          props.onAdd({ move: selectedMove, result: isWin ? 'Win' : 'Lose' })
-        }
-      >
-        Add
-      </button>
-    </div>
-  );
-}
-
-// RPSButton.js
-function RPSButton(props) {
-  const [isHovered, setIsHovered] = React.useState(false);
-  const isEmoji = React.useContext(IsEmojiContext);
-  const winCount = props.records.filter(
-    (record) => record.result === 'Win'
-  ).length;
-  const totalCount = props.records.length;
-  const percentage = totalCount ? (winCount / totalCount) * 100 : 0;
-  return (
-    <button
-      onClick={props.onClick}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      {isEmoji ? emoji[props.move] : props.move}{' '}
-      {isHovered && `(${percentage.toFixed(2)}%)`}
-    </button>
-  );
-}
-
-function RPSButtons(props) {
-  return (
-    <div>
-      {moves.map((move, index) => (
-        <RPSButton
-          key={move}
-          onClick={() => props.onButtonPressed(move)}
-          move={move}
-          records={props.records.filter((record) => record.move === move)}
-        />
-      ))}
-    </div>
-  );
-}
-
-function RPSRecords(props) {
-  const isEmoji = React.useContext(IsEmojiContext);
-  let winPercentage = 0;
-  if (props.records.length)
-    winPercentage =
-      (props.records.filter((record) => record.result === 'Win').length /
-        props.records.length) *
-      100;
-
-  return (
-    <div>
-      <p>Rounds (Win %: {winPercentage.toFixed(2)}%):</p>
-      <ul>
-        {props.records.map((record, index) => (
-          <li key={index} onClick={() => props.onHide(index)}>
-            {record.result} ({isEmoji ? emoji[record.move] : record.move})
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-function RockPaperScissors(props) {
+export default function RockPaperScissors(props) {
   const [records, dispatch] = React.useReducer(recordsReducer, []);
   const [isEmoji, setIsEmoji] = React.useState(false);
+
   return (
     <IsEmojiContext.Provider value={isEmoji}>
       <div>
         <RPSButtons
           onButtonPressed={(move) => {
-            dispatch({ name: 'add', move });
+            dispatch({ name: 'add', move: move });
           }}
           records={records}
         />
-        <RPSInput onAdd={(record) => dispatch({ name: 'force add', record })} />
-        <button onClick={() => setIsEmoji(!isEmoji)}>Toggle Emoji</button>
+        <RPSInput onAdd={(record) => dispatch({ name: 'force add', record: record })} />
         <RPSRecords
           records={records}
           onHide={(index) => {
-            dispatch({ name: 'remove', index });
+            dispatch({ name: 'remove', index: index });
           }}
         />
+        <button onClick={() => setIsEmoji(!isEmoji)}>Toggle Emoji</button>
       </div>
     </IsEmojiContext.Provider>
   );
 }
 
-export default function App() {
-  return (
-    <div>
-      <RockPaperScissors />
-    </div>
-  );
-}
-
-
+//
 // export default function RockPaperScissors(props) {
 //     const [records, setRecords] = React.useState([]);
 //     return (
--- src/recordsReducer.jsx
+++ src/recordsReducer.jsx
@@ -1,23 +1,26 @@
+// recordsReducer.jsx
 function getRandomInt(min, max) {
-    return Math.round(Math.random() * (max - min)) + min;
+  return Math.round(Math.random() * (max - min)) + min;
+}
+
+export default function recordsReducer(currentRecords, action) {
+  switch (action.name) {
+    case 'add': {
+      const randomInt = getRandomInt(0, 3);
+      let result;
+      if (randomInt === 0) result = 'Win';
+      else if (randomInt === 1) result = 'Lose';
+      else result = 'Tie';
+
+      return [...currentRecords, { result: result, move: action.move }];
+    }
+    case 'force add': {
+      return [...currentRecords, action.record];
+    }
+    case 'remove': {
+      return currentRecords.filter((_, index) => index !== action.index);
+    }
+    default:
+      return currentRecords;
   }
-  
-  export default function recordsReducer(currentRecords, action) {
-        switch (action.name) {
-        case 'add': {
-          // copy-pasted from RockPaperScissors component, remember to copy getRandomInt definition too
-          const randomInt = getRandomInt(0, 3);
-  
-          let result;
-          if (randomInt === 0) result = 'Win';
-          else if (randomInt === 1) result = 'Lose';
-          else result = 'Tie';
-  
-          // We expect the move to be provided along with the action (e.g. dispatch({ name: add, move: 'Rock' }))
-          return [...currentRecords, { result: result, move: action.move }];
-        }
-        case 'force add': {}
-        case 'remove': {}
-        default: return currentRecords;
-      }
-  }
+}
